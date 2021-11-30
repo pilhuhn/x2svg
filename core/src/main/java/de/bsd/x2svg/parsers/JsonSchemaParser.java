eba04202f259fc51fe8ba78ffa3603d0c8451131
Heiko W. Rupp <hwr@pilhuhn.de>
2021-11-30 17:41:29 +0100

    Allow pulling in references from other files.

--- core/src/main/java/de/bsd/x2svg/parsers/JsonSchemaParser.java
+++ core/src/main/java/de/bsd/x2svg/parsers/JsonSchemaParser.java
@@ -9,6 +9,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.io.File;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -26,11 +27,15 @@ public class JsonSchemaParser implements InputParser {
     private final Log log = LogFactory.getLog(JsonSchemaParser.class);
 
     JsonNode jsonRootNode;
+    String rootPath;
 
     Set<String> primitiveTypes = Set.of("string","number","integer");
+    private boolean keepRoot = false;
 
     @Override
     public Container parseInput() throws Exception {
+
+        rootPath = schemaFile.getParent() + "/";
 
         log.info("Input " + schemaFile.getAbsolutePath());
         jsonRootNode = JsonLoader.fromFile(schemaFile);
@@ -44,11 +49,20 @@ public class JsonSchemaParser implements InputParser {
         }
 
         getContainer(jsonRootNode, rootContainer, "properties");
+        if (keepRoot) {
+            return rootContainer;
+        }
+        if (rootContainer.children.size()==1) {
+            Container c = rootContainer.children.get(0);
+            c.parent = null;
+            return c;
+        }
         return rootContainer;
     }
 
     private Container getContainer(JsonNode root, Container parentContainer, String keyToSearch) {
         JsonNode props = root.get(keyToSearch);
+        JsonNode ref = root.get("$ref");
         if (props != null) {
             Iterator<String> iter = props.fieldNames();
             while (iter.hasNext()) {
@@ -58,7 +72,6 @@ public class JsonSchemaParser implements InputParser {
                 parentContainer.children.add(type);
                 type.parent = parentContainer;
                 type.name = fieldName;
-//                System.out.println("Name " + fieldName + " type " + element.getNodeType().name());
 
                 if (element.isArray()) {
                     type.cardinality = new Cardinality("0", "*"); // TODO that is of the inner elements?
@@ -134,24 +147,53 @@ public class JsonSchemaParser implements InputParser {
                 }
             }
         }
-        JsonNode ref = root.get("$ref");
-        if (ref != null) {
+
+        else if (ref != null) {
             Container containerViaRef = getContainerViaRef(ref, parentContainer );
             parentContainer.content = ContentModel.SUBSTITUTION; // TODO substitution group?
             return containerViaRef;
         }
-        if (root.fieldNames().hasNext() && root.fieldNames().next().equals("enum")) {
+        else if (contains(root.fieldNames(),"enum")) {
             JsonNode enumNode = root.get("enum");
-            System.out.println(enumNode.toPrettyString());
+            System.out.println("Enum content: " + enumNode.toPrettyString());
             Container enumContainer = new Container();
             enumContainer.name="<" + enumNode.toPrettyString() + ">";
             enumContainer.parent = parentContainer;
             parentContainer.content = ContentModel.CHOICE;
             parentContainer.children.add(enumContainer);
-
+        }
+        else if (contains(root.fieldNames(), "type")) {
+            if (ref==null) {
+                System.err.println("Ref is null ");
+                getContainer(root, parentContainer, "properties");
+            } else {
+                System.out.println("Not yet handled ref " + ref.asText());
+            }
+        }
+        else {
+            log.error("Case not yet handled");
         }
 
         return parentContainer;
+    }
+
+    /**
+     * Return true if one of the iterated over Strings is the one to be found
+     * @param iter An iterator over Strings
+     * @param fieldToFind The string to search
+     * @return True if the string was found, false otherwise
+     */
+    boolean contains(Iterator<String> iter, String fieldToFind) {
+        if (!iter.hasNext()) {
+            return false;
+        }
+        while (iter.hasNext()) {
+            String field = iter.next();
+            if (field.equals(fieldToFind)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private String getExtras(JsonNode element) {
@@ -167,21 +209,27 @@ public class JsonSchemaParser implements InputParser {
     }
 
     private Container getContainerViaRef(JsonNode ref, Container parent) {
+        JsonNode deref=null;
+
         // This refers to a type definition somewhere else
         String reference = ref.asText();
+        String refShortName;
         // Now look this up via json pointer
         if (reference.startsWith("#")) {
             // Local reference within the document
             reference = reference.substring(1);
+
+            if (reference.contains(".")) {
+                refShortName = reference.substring(reference.lastIndexOf(".") + 1);
+            } else {
+                refShortName = reference.substring(reference.lastIndexOf("/") + 1);
+            }
+            deref = jsonRootNode.at(reference);
+        } else {
+            JsonNode node = getNodeFromUrl(reference);
+            refShortName = getShortNameFromUrl(reference);
+            deref = node;
         }
-        String refShortName;
-        if (reference.contains(".")) {
-            refShortName = reference.substring(reference.lastIndexOf(".") + 1);
-        }
-        else {
-            refShortName = reference.substring(reference.lastIndexOf("/") +1);
-        }
-        JsonNode deref = jsonRootNode.at(reference);
 
         Container refNameContainer = new Container();
         refNameContainer.name = refShortName;
@@ -200,12 +248,74 @@ public class JsonSchemaParser implements InputParser {
 
     @Override
     public void setParserOptions(String[] options) {
-        // TODO: Customise this generated block
+        for (String opt : options) {
+            switch (opt) {
+                case "-kr":
+                    keepRoot = true;
+                    break;
+                default:
+                    log.warn("Unknown option >" + opt + "< ignored");
+            }
+        }
+    }
+
+    /*
+     * Return a JsonNode from an "url".
+     * The URL is for the moment not fetched remotely, but the data is
+     * supposed to exist in the local file system. For
+     * this purpose, the path is evaluated and taken as local file path
+     */
+    JsonNode getNodeFromUrl(String urlString)  {
+
+        JsonNode node;
+        try {
+            URL u = new URL(urlString);
+            String path = u.getPath();
+            if (path.contains("/")) {
+                path = path.substring(path.lastIndexOf('/') +1);
+            }
+
+            // prepend the working dir
+            path = rootPath + path;
+
+            node = JsonLoader.fromPath(path);
+
+            // If we have a #ref at the end, we select that node
+            if (u.getRef() != null && !u.getRef().isBlank()) {
+                node = node.get(u.getRef());
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        if (node==null) {
+            throw new IllegalArgumentException("Bla bla, need a better message");
+
+        }
+
+        return node;
+
+    }
+
+    String getShortNameFromUrl(String urlString) {
+        try {
+            URL u = new URL(urlString);
+            if (u.getRef() != null && !u.getRef().isBlank()) {
+                return u.getRef();
+            }
+
+            String path = u.getPath();
+            path = path.substring((path.lastIndexOf('/') + 1));
+            return path;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public String getSpecificHelp() {
-        return "Nothing to see yet";
+        return "-kr: keep root object (filename/id) even if it has only one child";
     }
 
     @Override
